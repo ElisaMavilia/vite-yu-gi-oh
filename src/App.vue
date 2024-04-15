580bb828f3a04f43d14fdf1e6aa418e9ab93ffb2
Elisa <90789223+ElisaMavilia@users.noreply.github.com>
2024-04-15 14:13:48 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="text-center">
+    ciao!
+  </h1>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/assets/main.js
@@ -0,0 +1,6 @@
+import './styles/general.scss'
+
+import { createApp } from 'vue'
+import App from '/src/App.vue'
+
+createApp(App).mount('#app')
